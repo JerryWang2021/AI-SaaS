6c609b3a5ef270a96d648c30b3983e0d7bca1693
Zhi Wang <jerrywang65@hotmail.com>
2023-08-27 19:58:16 -0700

    complement landing page

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function LandingLayout({ children }: Props) {
+  return (
+    <main className="h-full bg-[#111827] overflow-auto">
+      <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
+    </main>
+  );
+}
